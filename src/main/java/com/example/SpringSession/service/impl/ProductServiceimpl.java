4723bd02ab75913e44b0bfa673aa05688a8b6990
Preetham Jain <preethamjain@Preethams-MacBook-Pro.local>
2021-02-23 14:02:13 +0530

    v4-first-commit(probably)

--- src/main/java/com/example/SpringSession/service/impl/ProductServiceimpl.java
+++ src/main/java/com/example/SpringSession/service/impl/ProductServiceimpl.java
@@ -9,28 +9,100 @@ import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.*;
 
 @Service
-public class ProductServiceimpl implements ProductService {
+public class ProductServiceimpl implements ProductService, Callable<List<ProductDTO>> {
     @Autowired
     private SearchClient searchClient;
     @Override
     public ProductResponseDTO getProducts(ProductRequestDTO request) {
-        
-        Map<String, Object> products = searchClient.getProducts(request.getSearchTerm());
-        List<Map<String, Object>> resp = (List<Map<String, Object>>) ((Map)products.get("response")).get("docs");
 
+        ProductResponseDTO responseDTO = new ProductResponseDTO();
+        ExecutorService threadPool = Executors.newFixedThreadPool(2);
+
+        threadPool.execute(() -> {
+//            System.out.println(Thread.currentThread().getName());
+            String searchTermQuery = request.getSearchTerm();
+            List<ProductDTO> productDTOS = getSearchTermBaseProducts(searchTermQuery);
+            responseDTO.setProducts(productDTOS);
+        });
+
+        threadPool.execute(() -> {
+//            System.out.println(Thread.currentThread().getName());
+            String locationQuery = "stockLocation" + ":\"" + request.getStockLocation() + "\"";
+            List<ProductDTO> locationProductDTOs = getSearchTermBaseProducts(locationQuery);
+            responseDTO.setLocationBasedProducts(locationProductDTOs);
+        });
+
+        awaitTerminationAfterShutdown(threadPool);
+        return responseDTO;
+
+//        String searchTermQuery = request.getSearchTerm();
+//        String locationQuery = "stockLocation: " + request.getStockLocation();
+//        ProductResponseDTO responseDTO = new ProductResponseDTO();
+//
+//        ExecutorService executorService = Executors.newFixedThreadPool(2);
+//
+//        Set<Callable<List<ProductDTO>>> callables = new HashSet<Callable<List<ProductDTO>>>();
+//
+//        callables.add(new Callable<List<ProductDTO>>() {
+//            @Override
+//            public List<ProductDTO> call() throws Exception {
+//                List<ProductDTO> productDTOS = getSearchTermBaseProducts(searchTermQuery);
+//                return productDTOS;
+//            }
+//        });
+//        callables.add(new Callable<List<ProductDTO>>() {
+//            @Override
+//            public List<ProductDTO> call() throws Exception {
+//                List<ProductDTO> locationProductDTOS = getSearchTermBaseProducts(locationQuery);
+//                return locationProductDTOS;
+//            }
+//        });
+//
+//        try {
+//            List<Future<List<ProductDTO>>> futures = executorService.invokeAll(callables);
+//            for(Future<List<ProductDTO>> future: futures) {
+//
+//                responseDTO.setProducts(productDTOS);
+//                responseDTO.setLocationBasedProducts(locationProductDTOS);
+//                System.out.println(future.get());
+//            }
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        } catch (ExecutionException e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//
+//
+//        return responseDTO;
+    }
+
+    private void awaitTerminationAfterShutdown(ExecutorService threadPool) {
+        threadPool.shutdown();
+        try {
+            if (!threadPool.awaitTermination(60, TimeUnit.SECONDS)) {
+                threadPool.shutdownNow();
+            }
+        } catch (InterruptedException ex) {
+            threadPool.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private List<ProductDTO> getSearchTermBaseProducts(String query) {
+        Map<String, Object> products = searchClient.getProducts(query);
+        List<Map<String, Object>> resp = (List<Map<String, Object>>) ((Map) products.get("response")).get("docs");
         List<ProductDTO> productDTOS = new ArrayList<>();
-        for (Map<String, Object> productClientResponse :resp) {
+        for (Map<String, Object> productClientResponse : resp) {
             String title = (String) productClientResponse.get("name");
-            boolean inStock = (int) productClientResponse.get("isInStock") == 1? true: false;
+            boolean inStock = (int) productClientResponse.get("isInStock") == 1 ? true : false;
             String description = (String) productClientResponse.get("description");
             double salePrice = (double) productClientResponse.get("salePrice");
-
             ProductDTO productDTO = new ProductDTO();
             productDTO.setSalesPrice(salePrice);
             productDTO.setInStock(inStock);
@@ -39,48 +111,11 @@ public class ProductServiceimpl implements ProductService {
 
             productDTOS.add(productDTO);
         }
+        return productDTOS;
+    }
 
-        ProductResponseDTO responseDTO = new ProductResponseDTO();
-        responseDTO.setProducts(productDTOS);
-
-        String location = "stockLocation:" + request.getStockLocation();
-        Map<String, Object> locationProducts = searchClient.getProducts(location);
-        List<Map<String, Object>> locationResponse = (List<Map<String, Object>>) ((Map)locationProducts.get("response")).get("docs");
-
-        List<ProductDTO> locationProductDTOS = new ArrayList<>();
-        for (Map<String, Object> productClientResponse :locationResponse) {
-            String title = (String) productClientResponse.get("name");
-            boolean inStock = (int) productClientResponse.get("isInStock") == 1? true: false;
-            String description = (String) productClientResponse.get("description");
-            double salePrice = (double) productClientResponse.get("salePrice");
-
-            ProductDTO productDTO = new ProductDTO();
-            productDTO.setSalesPrice(salePrice);
-            productDTO.setInStock(inStock);
-            productDTO.setTitle(title);
-            productDTO.setDescription(description);
-
-            locationProductDTOS.add(productDTO);
-        }
-
-        responseDTO.setLocationBasedProducts(locationProductDTOS);
-
-        return responseDTO;
-
-
-//
-//        ProductResponseDTO responseDTO = new ProductResponseDTO();
-//        ProductDTO product = new ProductDTO();
-//        product.setDescription((String) resp.get(0).get("description"));
-//        if((int)resp.get(0).get("isInStock") == 1) {
-//            product.setInStock(true);
-//        }
-//        else {
-//            product.setInStock(false);
-//        }
-//        product.setSalesPrice((double) resp.get(0).get("salePrice"));
-//        product.setTitle((String) resp.get(0).get("name"));
-//        responseDTO.setProducts(Arrays.asList(product));
-//        return responseDTO;
+    @Override
+    public List<ProductDTO> call() throws Exception {
+        return null;
     }
 }
